9a24be45b74023984e6ab55dc5d51d77a3cd3c16
dare babalola <darebabalola92@gmail.com>
2023-10-27 10:18:39 +0100

    added console logs

--- controllers/users.js
+++ controllers/users.js
@@ -398,8 +398,10 @@ router.get('/getUsers', async (req, res) => {
       res.status(404).json('No user found')
     }
     res.status(200).json(foundUsers)
+    console.log(foundUsers)
   } catch (err) {
     res.status(500).json(err)
+    console.log(err)
   }
 })
 module.exports = router
